3f9eaec95f77d6ef81428c937e9f46f5bdc2c4c9
asha <aasha.udupa@gmail.com>
2016-08-29 20:02:12 +0800

    first commit

---
+++ src/seedu/addressbook/commands/ClearCommand.java
@@ -0,0 +1,25 @@
+package seedu.addressbook.commands;
+
+import seedu.addressbook.common.Utils;
+
+
+/**
+ * Clears the address book.
+ */
+public class ClearCommand extends Command {
+
+    public static final String COMMAND_WORD = "clear";
+    public static final String MESSAGE_USAGE = "Clears address book permanently.\n"
+            + "Example: " + COMMAND_WORD;
+
+    public static final String MESSAGE_SUCCESS = "Address book has been cleared!";
+
+    public ClearCommand() {}
+
+
+    @Override
+    public CommandResult execute() {
+        addressBook.clear();
+        return new CommandResult(MESSAGE_SUCCESS);
+    }
+}
